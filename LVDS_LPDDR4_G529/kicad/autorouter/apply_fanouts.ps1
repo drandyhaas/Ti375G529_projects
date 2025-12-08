47d20f403c938df9fae68035c3b221088447f3e1
Andy <drandyhaas@gmail.com>
2025-12-08 13:01:22 -0500

    A bga fanout test for a few chips

---
+++ LVDS_LPDDR4_G529/kicad/autorouter/apply_fanouts.ps1
@@ -0,0 +1,20 @@
+# Test fanning out BGA chips
+# Run in Windows powershell like: .\apply_fanouts.ps1
+
+# Remaining FTDI nets (the DATA ones are already in the fanout_starting_point file, so we check-for-previous)
+python bga_fanout.py fanout_starting_point.kicad_pcb --component U3 --output routed_output.kicad_pcb --nets "*U2A*" --primary-escape horizontal --check-for-previous --force-escape-direction
+
+# LVDS from ADC
+python bga_fanout.py routed_output.kicad_pcb --component IC1 --output routed_output.kicad_pcb --nets "*lvds_rx*" --diff-pairs "*lvds_rx*" --primary-escape vertical
+
+# LVDS on FPGA
+python bga_fanout.py routed_output.kicad_pcb --component U3 --output routed_output.kicad_pcb --nets "*lvds_rx*" --diff-pairs "*lvds_rx*" --primary-escape vertical
+
+# DDR on FPGA
+python bga_fanout.py routed_output.kicad_pcb --component U3 --output routed_output.kicad_pcb --nets "*U1A*" --primary-escape horizontal
+
+# DDR on DDR chip
+python bga_fanout.py routed_output.kicad_pcb --component U1 --output routed_output.kicad_pcb --nets "*U1A*" --primary-escape horizontal
+
+# Check for errors
+python.exe .\check_drc.py .\routed_output.kicad_pcb
